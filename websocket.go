ea23366ae953977da19582d3e74d28d566923317
wangoo <wongoo@apache.org>
2020-12-08 09:01:22 +0800

    rft: rename

--- websocket.go
+++ websocket.go
@@ -17,7 +17,26 @@ func (ww *WebsocketTransfer) Trans(bytes []byte) error {
 	return ww.conn.WriteMessage(1, bytes)
 }
 
-const MessageTypeConfigMatcher = '1'
+func startWebsocketTransfer(response http.ResponseWriter, request *http.Request, serverId string) {
+	c, err := upgrader.Upgrade(response, request, nil)
+	if err != nil {
+		logger.Error("web socket error:", err)
+		return
+	}
+	defer c.Close()
+
+	server, ok := serverDB[serverId]
+	if !ok {
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+	websocketTransfer := &WebsocketTransfer{conn: c}
+	router := NewRouter(nil, []Transfer{websocketTransfer})
+	server.StartRouter(router)
+	startWebsocketHeartbeat(router, websocketTransfer)
+}
+
+const MessageTypeMatcherConfig = '1'
 
 func startWebsocketHeartbeat(router *Router, transfer *WebsocketTransfer) {
 	defer func() {
@@ -39,35 +58,16 @@ func startWebsocketHeartbeat(router *Router, transfer *WebsocketTransfer) {
 				return
 			}
 
-			if len(data) > 0 && data[0] == MessageTypeConfigMatcher {
-				if err := handleRouterConfig(router, data[1:]); err != nil {
-					logger.Warnf("router %s websocket router config error: %+v", router.id, err)
+			if len(data) > 0 && data[0] == MessageTypeMatcherConfig {
+				if err := handleMatcherConfigUpdate(router, data[1:]); err != nil {
+					logger.Warnf("router %s websocket matcher config error: %+v", router.id, err)
 				}
 			}
 		}
 	}
 }
 
-func startWebsocketTransfer(response http.ResponseWriter, request *http.Request, serverId string) {
-	c, err := upgrader.Upgrade(response, request, nil)
-	if err != nil {
-		logger.Error("web socket error:", err)
-		return
-	}
-	defer c.Close()
-
-	server, ok := serverDB[serverId]
-	if !ok {
-		response.WriteHeader(http.StatusNotFound)
-		return
-	}
-	websocketTransfer := &WebsocketTransfer{conn: c}
-	router := NewRouter(nil, []Transfer{websocketTransfer})
-	server.StartRouter(router)
-	startWebsocketHeartbeat(router, websocketTransfer)
-}
-
-func handleRouterConfig(router *Router, data []byte) error {
+func handleMatcherConfigUpdate(router *Router, data []byte) error {
 	var matcherConfigs []*MatcherConfig
 	if err := json.Unmarshal(data, &matcherConfigs); err != nil {
 		return err
