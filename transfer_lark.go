a2ebc92b19d643b3024de9cfc86a3d3387857533
ukuz90 <zhuangjinjin90@gmail.com>
2021-11-15 10:37:52 +0800

    refactor transfer_lark and delete transfer_im

--- transfer_lark.go
+++ transfer_lark.go
@@ -17,29 +17,82 @@
 
 package logtail
 
-const TransferTypeLark = "feishu"
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/vogo/logger"
+)
 
 type LarkTransfer struct {
-	delegateTransfer Transfer // delegate
+	url                      string
+	transferring             int32 // whether transferring message
+	messageDataFixedBytesNum int
+	messageDataMaxLength     int
+	messageDataPrefix        []byte
+	messageDataSuffix        []byte
+	messageTransferInterval  time.Duration
 }
 
 const (
 	larkMessageDataFixedBytesNum = 4
 	larkMessageDataMaxLength     = 1024
+	TransferTypeLark             = "feishu"
+	larkMessageTransferInterval  = time.Second * 5
 )
 
 func (d *LarkTransfer) start(*Router) error { return nil }
 
 func (d *LarkTransfer) Trans(serverID string, data ...[]byte) error {
-	return d.delegateTransfer.Trans(serverID, data...)
+	if !atomic.CompareAndSwapInt32(&d.transferring, 0, 1) {
+		// ignore message to
+		return nil
+	}
+
+	go func() {
+		<-time.After(d.messageTransferInterval)
+		atomic.StoreInt32(&d.transferring, 0)
+	}()
+
+	size := d.messageDataFixedBytesNum + len(data)
+	list := make([][]byte, size)
+	list[0] = d.messageDataPrefix
+	list[1] = []byte(serverID)
+	list[2] = messageTitleContentSplit
+
+	idx := 3
+	messageRemainCapacity := d.messageDataMaxLength
+
+	for _, b := range data {
+		if messageRemainCapacity <= 0 {
+			break
+		}
+
+		b = EscapeLimitJSONBytes(b, messageRemainCapacity)
+
+		list[idx] = b
+		idx++
+
+		messageRemainCapacity -= len(b)
+	}
+
+	list[idx] = d.messageDataSuffix
+
+	if err := httpTrans(d.url, list[:idx+1]...); err != nil {
+		logger.Errorf("ding error: %v", err)
+	}
+
+	return nil
 }
 
 func NewLarkTransfer(url string) Transfer {
 	return &LarkTransfer{
-		delegateTransfer: NewImTransfer(url,
-			larkMessageDataFixedBytesNum,
-			larkMessageDataMaxLength,
-			larkTextMessageDataPrefix,
-			larkTextMessageDataSuffix),
+		url:                      url,
+		transferring:             0,
+		messageDataFixedBytesNum: larkMessageDataFixedBytesNum,
+		messageDataMaxLength:     larkMessageDataMaxLength,
+		messageDataPrefix:        larkTextMessageDataPrefix,
+		messageDataSuffix:        larkTextMessageDataSuffix,
+		messageTransferInterval:  larkMessageTransferInterval,
 	}
 }
