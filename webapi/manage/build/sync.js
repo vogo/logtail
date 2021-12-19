e922d310794266d14abd8bb03f12eaa676d8b93b
xuyimingwork <xuyimingwork@foxmail.com>
2021-12-19 13:08:20 +0800

    fix: lint check for destructuring binding

--- webapi/manage/build/sync.js
+++ webapi/manage/build/sync.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var { resolve } = require('path');
+var resolve = require('path').resolve;
 
 function copy(src, dist) {
   fs.createReadStream(src).pipe(fs.createWriteStream(dist));
