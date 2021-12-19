897461099a8cde46f925f51eebb422eb2647a3b1
xuyimingwork <xuyimingwork@foxmail.com>
2021-12-19 13:05:47 +0800

    fix: lint check

--- webapi/manage/build/sync.js
+++ webapi/manage/build/sync.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const { resolve } = require('path');
+var fs = require('fs');
+var { resolve } = require('path');
 
 function copy(src, dist) {
   fs.createReadStream(src).pipe(fs.createWriteStream(dist));
 }
 
-copy(resolve(__dirname, '../dist/index.html'), resolve(__dirname, '../../manage.html'))
+copy(resolve(__dirname, '../dist/index.html'), resolve(__dirname, '../../manage.html'));
